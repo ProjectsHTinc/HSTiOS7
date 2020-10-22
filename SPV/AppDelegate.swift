e30662da7987ac863e33dbfdd08c7b7978a68d0d
HappySanzTech <admin@palprotech.com>
2020-10-22 18:40:33 +0530

    my code

--- SPV/AppDelegate.swift
+++ SPV/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print ("karan testt")
+        print ("karan tes")
         // Override point for customization after application launch.
         return true
     }
